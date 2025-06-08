60ac7775614f4b21820c3957c6d939d88390e53d
margOnline <margourey@gmail.com>
2025-06-08 18:09:41 +0100

    Add navigation using file based routing with bottom tabs

---
+++ app/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign'
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
+import { theme } from "../theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Shopping List',
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="list" size={size} color={color} />
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: 'Counter',
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="clockcircleo" size={size} color={color} />
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: 'Idea',
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome5 name="lightbulb" size={size} color={color} />
+          },
+        }}
+      />
+    </Tabs>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { theme } from "./theme"
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import { theme } from "../theme"
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link href="/counter" style={{ textAlign: "center", marginBottom: 18}}>Counter</Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Milk" />
