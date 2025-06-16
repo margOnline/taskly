78d6970dba3b348d6f283fcd694c5c0dc077f8c4
margOnline <margourey@gmail.com>
2025-06-16 16:47:43 +0100

    Use TextInput and useState to enable entering an item and storing it for display

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,64 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { theme } from "../theme"
+import { StyleSheet, TextInput, View } from "react-native";
+import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { Link } from "expo-router";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue(undefined);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Link href="/counter" style={{ textAlign: "center", marginBottom: 18}}>Counter</Link>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Milk" />
-      <StatusBar style="auto" />
+      <TextInput
+        value={value}
+        style={styles.textInput}
+        onChangeText={setValue}
+        placeholder="E.g Coffee"
+        onSubmitEditing={handleSubmit}
+        returnKeyType="done"
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem name={item.name} key={item.id} />
+      ))}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+    flex: 1,
   },
-})
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    marginHorizontal: 12,
+    marginBottom: 12,
+  },
+});
