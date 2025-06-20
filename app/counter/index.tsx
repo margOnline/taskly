35e5e1937543cee933a621db863830627e6cd58f
Margo Urey <margourey@gmail.com>
2025-06-20 17:18:41 +0100

    Notifications (#6)

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,40 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import * as Device from "expo-device";
+import * as Notifications from 'expo-notifications'
+import { registerForPushNotificationsAsync } from '../utils/registerForPushnotificationsAsync';
+import { theme } from '../../theme';
 
 export default function CounterScreen() {
+  const scheduleNotificaton = async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app 📢"
+        },
+        trigger: {
+          seconds: 5,
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL
+        }
+      });
+    }  else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable the notification permission in settings for Expo Go"
+        );
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={scheduleNotificaton}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -15,7 +46,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 24,
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
   },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  }
 })
